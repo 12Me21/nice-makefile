770f685b352e45acca5fe8d537bd823ddc613c53
12Me21 <12Me21.mc@gmail.com>
2021-01-25 13:01:30 -0500

    include string.h

--- heck.c
+++ heck.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include "types.h"
 
 void heck(Str arg) {
     Str pos = arg;
-    while (pos = strstr(pos, "hell"))
+    while ((pos = strstr(pos, "hell")))
         memcpy(pos, "heck", 4);
     printf("%s\n", arg);
 }
