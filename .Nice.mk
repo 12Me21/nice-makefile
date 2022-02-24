2ca1d9ab34192c2566e9b59cfb006603bed8450e
12Me21 <12Me21.mc@gmail.com>
2022-02-24 07:13:02 -0500

    commiting old changes

--- .Nice.mk
+++ .Nice.mk
@@ -1,37 +1,47 @@
 # rebuild everything when the makefile is modified
-.EXTRA_PREREQS+= Makefile .Nice.mk
+.EXTRA_PREREQS += Makefile .Nice.mk
 
 # disable builtin rules to improve speed
-MAKEFLAGS+= --no-builtin-rules
+MAKEFLAGS += --no-builtin-rules
 .SUFFIXES:
+
+# delete target files when an error occurs
+.DELETE_ON_ERROR:
 
 # location for intermediate files (.o and .mk)
 # (will be created automatically, as well as any subdirectories)
 # (ex: src `subdir/file` will create `.junk/subdir/` and compile `subdir/file.c` to `.junk/subdir/file.o`)
-junkbase:= .junk
-ifdef junkdir
- junkdir:= $(junkbase)/$(junkdir)
-else
- junkdir:= $(junkbase)
-endif
-srcdir?= .
+junkbase := .junk
+junkdir := $(junkbase)/$(output)
+srcdir ?= .
 
 # print status nicely (assumes ansi-compatible terminal)
-empty:=
-comma:= ,
-printlist = [$1m$(subst $(empty) $(empty),[m$(comma) [$1m,$(2:$3%=[$1m%))
-print = echo '$(call printlist,33,$1,$2)	[37mfrom: $(call printlist,32,$3,$4)[m'
+empty :=
+comma := ,
+printlist = [$1m$(subst $(empty) $(empty),[39m$(comma) [$1m,$(2:$3%=[$1m%))
+print = echo '[48;5;230;0m[K$(call printlist,33,$1,$2)	[37mfrom: $(call printlist,32,$3,$4)[m'
+
+ifdef pkgs
+ # check if the required packages are installed
+ $(shell pkg-config --print-errors --short-errors --exists $(pkgs))
+ ifneq ($(.SHELLSTATUS),0)
+  $(error MISSING PACKAGES?)
+ endif
+ # then get the flags
+ CFLAGS += $(shell pkg-config --cflags $(pkgs))
+ libflags := $(shell pkg-config --libs $(pkgs))
+endif
 
 # Link
 $(output): $(srcs:%=$(junkdir)/%.o)
 	@$(call print,$@,,$^,$(junkdir)/)
-	@$(CC) $(LDFLAGS) $^ $(libs:%=-l%) -o $@
+	@$(CC) $^ $(libflags) $(libs:%=-l%) -o $@
 
 # Compile
 # (this should be a grouped target ( &: instead of : ) but,
 #  that is only supported in newer versons of make,
 #  which most people don't have)
-$(junkdir)/%.o $(junkdir)/%.mk : $(srcdir)/%.c
+$(junkdir)/%.o $(junkdir)/%.mk : $(srcdir)/%
 	@mkdir -p $(@D)
 	@$(call print,$(junkdir)/$*.o,$(junkdir)/,$^,$(srcdir)/)
 # I replace `-I...` with `-isystem...` here, which will mark these
@@ -39,12 +49,15 @@ $(junkdir)/%.o $(junkdir)/%.mk : $(srcdir)/%.c
 # this is a hack, and I'm not sure how safe it is...
 # (If this breaks, the next best option would be to strip
 #  certain entries from the dependency file, after it is generated)
-	@$(CC) $(CFLAGS:-I%=-isystem%) -MMD -MF$(junkdir)/$*.mk -MQ$(junkdir)/$*.mk -MQ$(<:%.c=$(junkdir)/%.o) -c $< -o $(junkdir)/$*.o
+	@$(CC) $(CFLAGS:-I%=-isystem%) $(defines:%=-D%) -MMD -MP -MF$(junkdir)/$*.mk -MQ$(junkdir)/$*.mk -MQ$(<:%=$(junkdir)/%.o) -c $< -o $(junkdir)/$*.o
 
 .PHONY: clean
 clean:
-	$(RM) -r $(junkdir)
+	$(RM) -r $(junkbase)
 	$(RM) $(output)
+ifdef clean_extra # extra user defined files to be cleaned
+	$(RM) $(clean_extra)
+endif
 
 ifneq ($(findstring clean,$(MAKECMDGOALS)),)
  #disable multiple jobs when clean is running, since that will break.
@@ -55,6 +68,8 @@ else
 # we skip including these files, because that will be redundant.
 # (and it would mess with `clean`, because Make will try to
 #  generate these included files if they don't exist)
+
+# alright this is also a problem when running make uninstall etc. ugh.
   include $(srcs:%=$(junkdir)/%.mk)
  endif
 endif
--- Makefile
+++ Makefile
@@ -1,20 +1,14 @@
 # executable name
-output:= a.out
-# source files (without .c extension)
-srcs:= heck main
+output := a.out
+# source files
+srcdir := example-src
+srcs := heck.c main.c
 
 # compiler flags
-CFLAGS+= -Wall -Wextra -Os -g
+CFLAGS += -Wall -Wextra -Os -g
 # linker flags
 #LDFLAGS+= -lX11 -llua5.3
-# shortcut for -l arguments:
-libs:= X11 lua5.3
-
-#(haven't tested this!)
-#srcdir:= src
-
-#this will put the files in .junk/whatever
-#junkdir:= whatever
+pkgs := x11 lua5.3
 
 ######################################################################
 
